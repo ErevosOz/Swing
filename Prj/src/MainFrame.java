5d59fca2156a16b2051ab213283a9de6c4e03afd
ErevosOz <gobland@mail.ru>
2015-02-05 14:42:26 +0200

    test

--- Prj/src/MainFrame.java
+++ Prj/src/MainFrame.java
@@ -1,14 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 
 public class MainFrame extends JFrame {
 
-//    private JTextArea textArea;
     private Textpanel textPanel;
     private JButton btn;
+    private Toolbar toolbar;
 
     public MainFrame(){
         super("Hello World");
@@ -18,19 +16,19 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        toolbar = new Toolbar();
         textPanel = new Textpanel();
-        btn = new JButton("Click me");
 
-
-        add(textPanel, BorderLayout.CENTER);
-        add(btn, BorderLayout.SOUTH);
-
-        btn.addActionListener(new ActionListener() {
+        toolbar.setStringListener(new StringListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                textPanel.appendText("Button clicked\n");
+            public void textEmmited(String text) {
+                textPanel.appendText(text);
             }
         });
+
+        add(toolbar, BorderLayout.NORTH);
+        add(textPanel, BorderLayout.CENTER);
+
 
 
     }
